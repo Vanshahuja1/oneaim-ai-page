f49fb4effc5c7529c8f3d72732dc1187ee423aec
Vanshahuja1 <139003501+Vanshahuja1@users.noreply.github.com>
2025-04-01 18:00:26 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Suite from "@/sections/suite";
-import  Hero  from "../sections/Hero";
-import Cards from "@/sections/cards";
+import  Hero  from "@/sections/Hero";
+import Cards from "@/sections/Cards";
 import LogoTicker from "@/sections/logoTicker";
 export default function Home(){
   return(
